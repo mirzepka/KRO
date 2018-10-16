bbd1a339b7ebcb4aaffe0c009f204d23eaadda44
student <rzepka1995@gmail.com>
2018-10-16 19:28:23 +0200

    2.lab

--- untitled/app/views.py
+++ untitled/app/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, render_to_response
+
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('a')
+    return render_to_response('temp.html',{'name':"VALUE"} )
 
 def category(request, id):
-    pass
+    return HttpResponse('CATEGORY')
