26eada2bda2a5c6e67105f016c75cad4b5e77294
Live session user <rzepka1995@gmail.com>
2018-11-20 18:17:42 +0000

    ImageProcessing

---
+++ ImageProcessing/1.py
@@ -0,0 +1,14 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+cv2.resizeWindow('image', 800, 600)
+
+img = cv2.imread('Data/water_coins.jpg',cv2.IMREAD_GRAYSCALE)
+
+cv2.imshow('image',img)
+cv2.waitKey(0)
+
+
+cv2.destroyAllWindows()
+
